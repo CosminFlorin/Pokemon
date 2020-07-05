f62664d8a128e9524380ec4e9fae2e2a60cce72e
CosminFlorin <mihalceacosminflorin@hotmail.com>
2020-07-05 15:45:07 +0300

    Updates

---
+++ precache-manifest.14d93e37e2e85e70ba67336e652efe0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992978dbea971da5ac834594ad5f0043",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "7432fef67e527551e939",
+    "url": "/Pokemon/static/js/2.04a8ec48.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Pokemon/static/js/2.04a8ec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c6ede7477cc33dd9095",
+    "url": "/Pokemon/static/js/main.387d58c4.chunk.js"
+  },
+  {
+    "revision": "e3e3bf90a0551c891a41",
+    "url": "/Pokemon/static/js/runtime-main.5b778144.js"
+  }
+]);
